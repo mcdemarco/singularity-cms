9d23b0b854fd779565c094d2a5f4900e6766ec30
M. C. DeMarco <mcd@mcdemarco.net>
2015-03-09 02:50:08 -0400

    Add directory index as button bar.

--- index.php
+++ index.php
@@ -22,8 +22,13 @@ if($url) $file = strtolower(CONTENT_DIR . $url);
 else $file = CONTENT_DIR .'index';
 
 // Load the file
-if(is_dir($file)) $file = CONTENT_DIR . $url .'/index' . $file_format;
-else $file .=  $file_format;
+if(is_dir($file)) {
+  $path = $url;
+  $file = CONTENT_DIR . $url .'/index' . $file_format;
+} else {
+  $path = substr($url, 0, strrpos($url, "/"));
+  $file .=  $file_format;
+}
 
 // Show 404 if file cannot be found
 if(file_exists($file)) $content = file_get_contents($file);
@@ -32,9 +37,33 @@ else $content = file_get_contents(CONTENT_DIR .'404' . $file_format);
 <!DOCTYPE html>
 <html>
 <head>
-<title><?php echo ucwords(strtolower($url)); ?></title>
+<title><?php echo $url; ?></title>
 </head>
+
 <xmp theme="<?php echo $bootswatch_theme; ?>" style="display:none;">
+
+<div class="btn-toolbar" role="toolbar" aria-label="grouping">
+  <div class="btn-group btn-group-xs" role="group" aria-label="home">
+     <button type="button" class="btn btn-default"><a href="/">home</a></button>
+  </div>
+  <div class="btn-group btn-group-xs" role="group" aria-label="thisDir">
+<?php
+$dir = new DirectoryIterator(CONTENT_DIR . $path);
+foreach ($dir as $fileinfo) {
+  $displayName = explode($file_format, $fileinfo)[0];
+  if (!$fileinfo->isDot() && ("." . $fileinfo->getExtension() == $file_format || $fileinfo->getExtension() == "") && ($displayName != "404")) {
+    ?>
+    <button type="button" class="btn btn-default"><a href="/<?php if($path != "") {echo $path; echo "/";} echo $displayName; ?>"><?php echo $displayName; if ($fileinfo->isDir()) echo "/"; ?></a></button>
+    <?php
+  }
+}
+?>
+  </div>
+  <div class="btn-group btn-group-xs" role="group" aria-label="up">
+    <button type="button" class="btn btn-default"><a href="../">up</a></button>
+  </div>
+</div>
+
 <?php echo $content; ?>
 </xmp>
 <script src="<?php echo $strapdown_location; ?>"></script>
